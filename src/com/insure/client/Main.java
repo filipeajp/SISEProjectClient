7beb2869e85fe31e2bc78ac25a05bd1f634296ce
filipeajp <59914252+filipeajp@users.noreply.github.com>
2020-02-03 18:38:39 +0000

    V1.0 - Client Server. First Updates

--- src/com/insure/client/Main.java
+++ src/com/insure/client/Main.java
@@ -1,7 +1,90 @@
 package com.insure.client;
+//  Criar Servidor
+// Mudar a dir to script
+// Correr no Terminal Wsimport.bat -s ..\src -keep -p com.insure.client http://localhost:8090/docstorage?wsdl
+
+import javax.swing.*;
+import javax.xml.ws.BindingProvider;
 
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Project template - client");
-    }
+	private static final String identifier = "Client";
+
+	/*
+	 * The main function initialize the remote instance and connects to the remote service
+	 * */
+	public static void main (String[] args) throws InterruptedException, ClaimNotFoundException_Exception, Exception_Exception {
+		ClaimDataStoreService claimdatastoreService = new ClaimDataStoreService();
+
+		ClaimDataStore claimDataStore = (ClaimDataStore) claimdatastoreService.getClaimDataStorePort();
+
+		((BindingProvider) claimDataStore).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+				"http://localhost:8090/docstorage");
+
+		runClaimDataBase(claimDataStore, identifier);
+
+	}
+
+	/*
+	 * This method executes the ClaimData client by receiving an expression as input
+	 * from the user with numbers, multiplications and summations and printing the result
+	 * ClaimDataStore will run until the user input 'exit'
+	 * */
+	public static void runClaimDataBase (ClaimDataStore claimDataStore, String identifier) throws ClaimNotFoundException_Exception, Exception_Exception {
+
+		// implement in this method
+
+		if (identifier.equals("Client")) {
+
+			String choice = JOptionPane.showInputDialog("Thank you for using our service. Select one of the following options \n [1] - Create a Claim \n [2] - Summit Documents\n [3] - List Documents \n  exit to finish");
+
+			while (true) {
+				if (choice.equals("1")) {
+					String claim_desc = JOptionPane.showInputDialog("Write down your claim");
+					claimDataStore.createClaim(claim_desc);
+					choice = JOptionPane.showInputDialog("Thank you for using our service. Select one of the following options \n [1] - Create a Claim \n [2] - Summit Documents\n [3] - List Documents \n  exit to finish");
+				} else if (choice.equals("2")) {
+					String claimId = JOptionPane.showInputDialog("Insert claim ID:");
+					String docName = JOptionPane.showInputDialog("Insert document name:");
+					String docContent = JOptionPane.showInputDialog("Insert document content :");
+
+					claimDataStore.createDocument(Integer.parseInt(claimId), docName, docContent, "sdf", "user1PrivateKey");
+					choice = JOptionPane.showInputDialog("Thank you for using our service. Select one of the following options \n [1] - Create a Claim \n [2] - Summit Documents\n [3] - List Documents \n  exit to finish");
+				} else if (choice.equals("3")) {
+					String claimId = JOptionPane.showInputDialog("Insert claim ID:");
+					Claim claim = claimDataStore.getClaim(Integer.parseInt(claimId));
+
+					JOptionPane.showMessageDialog(null, claimDataStore.listDocuments("user1", Integer.parseInt(claimId)).toString());
+
+					choice = JOptionPane.showInputDialog("Thank you for using our service. Select one of the following options \n [1] - Create a Claim \n [2] - Summit Documents\n [3] - List Documents \n  exit to finish");
+
+				} else {
+					break;
+				}
+			}
+		} else if (identifier.equals("Officer")) {
+
+			String choice = JOptionPane.showInputDialog("Ready to work. Select one of the following options \n [1] - Summit Documents\n [2] - List Documents \n  exit to finish");
+
+			while (true) {
+
+				if (choice.equals("1")) {
+
+				} else if (choice.equals("2")) {
+					String claim_desc = JOptionPane.showInputDialog("Document");
+
+
+				} else {
+					System.out.println("Exiting");
+					break;
+				}
+			}
+
+
+		}
+
+
+	}
+
+
 }
+
---
+++ src/com/insure/client/Signature.java
@@ -0,0 +1,59 @@
+package com.insure.client;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+public class Signature {
+	public Signature () {
+	}
+
+	private String generateHash (Document document) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		return Base64.getEncoder().encodeToString(digest.digest(document.toString().getBytes("UTF-8")));
+	}
+
+	private String encryptHash (String fileName, String hash) throws java.lang.Exception {
+		AsymEncryptPriv cipher = new AsymEncryptPriv();
+		PrivateKey privKey = cipher.getPrivate(fileName);
+		return cipher.encryptText(hash, privKey);
+	}
+
+	private String decryptHash (String fileName, String encryptedHash) throws java.lang.Exception {
+		AsymDecryptPub cipher = new AsymDecryptPub();
+		PublicKey pubKey = cipher.getPublic(fileName);
+		return cipher.decryptText(encryptedHash, pubKey);
+	}
+
+	// create hash(message) and encrypt hash
+	public String createSignature (String fileName, Document document) throws java.lang.Exception {
+		//create hash
+		String hash = generateHash(document);
+
+		// encrypt hash
+		String encryptedHash = encryptHash(fileName, hash);
+
+		return encryptedHash;
+
+	}
+
+	//decrypt hash and compare hash obtained from the message
+	// validate integrity and authenticity
+	public boolean validateSignature (String fileName, String encryptedHash, Document document) throws java.lang.Exception {
+		// Decrypt encrypted hash
+		String decryptedHash = decryptHash(fileName, encryptedHash);
+
+		// Create hash of document
+		String docHash = generateHash(document);
+
+		// Validate
+		if (!docHash.equals(decryptedHash)) return false;
+
+		return true;
+	}
+}
+
+
